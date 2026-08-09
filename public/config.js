0e31cfc1e33ff9f316fe6b9e30614dd60fa92729
mgavassa11 <mgavassa11@gmail.com>
2026-08-09 16:39:20 +0200

    Delete public/config.js

--- public/config.js
+++
@@ -1,5 +0,0 @@
-// ============================================================
-//  SUPABASE CONFIG — Liga de Tenis Sohail
-// ============================================================
-const SUPA_URL = "https://lhiglzrttnltsxaqyhtx.supabase.co";
-const SUPA_KEY = "sb_publishable_usn0DucHGt5s2Am_c8M1CA_dLFwIAq7";
